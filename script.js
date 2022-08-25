dc8f00d89d32faa8339331c6e5ebdd8a23c69afd
anna_k <ackongs@gmail.com>
2022-08-24 20:15:54 -0600

    added single button play js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const sounds = ['applause', 'boo', 'gasp', 'tada', 'wrong']
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
 
 sounds.forEach(sound => {
     const btn = document.createElement('button')
@@ -6,6 +6,22 @@ sounds.forEach(sound => {
 
     btn.innerText = sound
 
+    btn.addEventListener('click', () => {
+        stopSongs()
+
+        document.getElementById(sound).play()
+    })
+
     document.getElementById('buttons').
     appendChild(btn)
 })
+
+function stopSongs() {
+    sounds.forEach(sound => {
+        const song = document.getElementById
+        (sound)
+
+        song.pause()
+        song.currentTime = 0;
+    })
+}
